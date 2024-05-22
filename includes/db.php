06c2d5f590a69570903021abd27cf6c2ba86eb43
Monjur Akhter <47746095+monjur98@users.noreply.github.com>
2024-05-22 09:50:01 +0530

    Database is live

--- includes/db.php
+++ includes/db.php
@@ -1,9 +1,9 @@
 <?php
 
-$db['db_host'] = "localhost";
-$db['db_user'] = "root";
-$db['db_pass'] = "";
-$db['db_name'] = "cms";
+$db['db_host'] = "89.117.188.154";
+$db['db_name'] = "u122608919_phpcms";
+$db['db_user'] = "u122608919_phpcms";
+$db['db_pass'] = "h/np>MEnG:1";
 
 foreach($db as $key => $value){
     define(strtoupper($key), $value);
